51025f6fbc11ac2f3551c6739e6e54e5506f24da
Tatunoko0411 <241202@st.yoshida-g.ac.jp>
2025-06-20 11:22:46 +0900

    #1 コンソールで１－４の数字を入力できるように実装

---
+++ TrumpGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrumpGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] cards = new int[4];
+
+            //四回入力
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"{i+1}番目の数字を入力(1-4)>");
+
+                //チェック
+                if(!int.TryParse(Console.ReadLine(), out int input))
+                {
+                    Console.WriteLine("1-4で入力");
+                    i--;
+                    continue;
+                }
+
+                cards[i] = input;
+
+            }
+
+            foreach (int i in cards)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+            Console.ReadLine();
+            return;
+        }
+    }
+}
